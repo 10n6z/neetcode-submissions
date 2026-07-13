94af49948db1f98378812e86ba4225e9db40f6ba
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-13 17:50:37 +0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < numbers.size(); i++){
+            mp[numbers[i]] = i + 1;
+            if(mp.find(target - numbers[i]) != mp.end()) return {mp[target - numbers[i]], i + 1};
+        }
+    }
+};
