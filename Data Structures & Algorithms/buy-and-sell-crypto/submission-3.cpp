9f70f655ef082a36f02bc2e7390dd9add00afad2
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 01:28:38 +0300

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        vector<int> sell (prices.size() + 1);
+        sell[prices.size() - 1] = prices[prices.size() - 1];
+        for (int i = prices.size() - 2; i > 0; i--){
+            sell[i] = max(sell[i + 1], prices[i]);
+            cout << sell[i] << endl;
+        }
+        for (int i = 0; i < prices.size() - 1; i++){
+            profit = max(profit, sell[i + 1] - prices[i]);
+        }
+        return profit;
+    }
+};
