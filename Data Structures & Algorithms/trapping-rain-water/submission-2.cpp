cbdb06cefdabb06f2864a69ea3084479a67fb4ee
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 00:04:04 +0300

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        vector<int> l(height.size()), r(height.size());
+        l[0] = height[0];
+        r[height.size() - 1] = height[height.size() - 1];
+
+        for (int i = 1; i < height.size(); i++) {
+            l[i] = max(l[i-1], height[i]);
+            cout << l[i] << endl;
+        }
+
+        for (int i = height.size() - 2; i >= 0; i--) {
+            r[i] = max(r[i+1], height[i]);
+        }
+
+        for (int i = 0; i < height.size(); i++) {
+            ans += min(l[i], r[i]) - height[i];
+        }
+        return ans;
+    }
+};
