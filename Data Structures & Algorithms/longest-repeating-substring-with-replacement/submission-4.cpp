956d309a14e4691c4da1e010235c00054c43d644
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 17:18:01 +0300

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int longest = 1, length = 1, cnt = k, consider = 0;
+        unordered_map<char, int> mp;
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+            cnt = 1;
+            for (int j = i + 1; j < s.size(); j++) {
+                mp[s[j]]++;
+                cnt = max(cnt, mp[s[j]]);
+                if ((j - i + 1) - cnt <= k) longest = max(longest, j - i + 1);
+            }
+            mp.clear();
+        }
+        return longest;
+    }
+};
