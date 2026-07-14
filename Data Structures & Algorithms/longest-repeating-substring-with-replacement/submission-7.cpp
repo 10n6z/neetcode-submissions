da1c652243ead313970fab41773515b32921bd7f
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 17:44:52 +0300

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int longest = 1, length = 1, cnt = 0, l = 0;
+        unordered_map<char, int> mp;
+        for (int r = 0; r < s.size(); r++) {
+            mp[s[r]]++;
+            cnt = max(cnt, mp[s[r]]);    
+            while ((r - l + 1) - cnt > k) {
+                mp[s[l]]--;
+                l++;
+            }
+            longest = max(longest, r - l + 1);
+        }
+        return longest;
+    }
+};
