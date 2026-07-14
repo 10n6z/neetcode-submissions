47d0f5071b6ad3de5edc500f793dad40ebcb6167
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 18:45:57 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int cnt[30];
+        if (s.size() < t.size()) {
+            swap(s, t);
+        }
+        for (int i = 0; i < s.size(); i++) {
+            cnt[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            if(cnt[t[i] - 'a'] == 0) return false;
+            cnt[t[i] - 'a']--;
+        }
+        return true;
+    }
+};
