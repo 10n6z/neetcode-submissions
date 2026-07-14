c6a255abeca757eb653ccfa82ca2416c516ad380
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-14 19:37:36 +0300

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.size() < s1.size()) return false;
+        vector<int> cnt1(26, 0), cnt2(26, 0);
+        for (int i = 0; i < s1.size(); i++) {
+            cnt1[s1[i] - 'a']++;
+            cnt2[s2[i] - 'a']++;
+        }
+        int l = 0;
+        for (int r = s1.size(); r < s2.size(); r++) {
+            if (cnt1 == cnt2) return true;
+            cnt2[s2[l] - 'a']--;
+            cnt2[s2[r] - 'a']++;
+            l++;
+            cout << s2[l] << endl;
+        }
+        return cnt1 == cnt2;
+    }
+};
