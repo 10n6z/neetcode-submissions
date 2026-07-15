2c4218d2ce636ad4d876a5041df9eaec5d97b84f
Long Pham <150258942+10n6z@users.noreply.github.com>
2026-07-15 21:24:29 +0300

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int l = 0, r = m - 1, row = 0;
+        while (l <= r) {
+            int mid = (r + l) / 2;
+            cout << mid << endl;
+            if (matrix[mid][0] > target) r = mid - 1;
+            else if (matrix[mid][n - 1] < target) l = mid + 1;
+            else {
+                row = mid;
+                break;
+            }
+            cout << l << " " << r << endl;
+        }
+        cout << row << endl;
+        l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (r + l) / 2;
+            if (matrix[row][mid] == target) return true;
+            else if (matrix[row][mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return false;
+    }
+};
